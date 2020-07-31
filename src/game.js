04ddff42612edb828e67efcb8085802df420406d
Matthew Lane <themplane@gmail.com>
2020-07-31 10:18:22 -0700

    Create Player class. Implemented players in the game class.

--- src/game.js
+++ src/game.js
@@ -5,15 +5,22 @@ class Game{
       ['','',''],
       ['','','']
     ];
+    this.player1 = new Player('x', './assets/ruby.png', 'Ruby-Player', '#9f7451');
+    this.player2 = new Player('o', './assets/js-icon.webp', 'JS-Player', '#748540');
+    this.currentPlayer = this.player1;
+    this.player1.otherPlayer = this.player2;
+    this.player2.otherPlayer = this.player1;
   }
 
   clickTile(tileID){
     var row = 'tmb'.indexOf(tileID[0]);
     var column = 'lcr'.indexOf(tileID[1]);
-    console.log(`${row} ${column}`)
+    console.log(`${row} ${column}`);
     if (this.board[row][column] === '') {
-      console.log('claimed!')
-      this.board[row][column] = 'x'
+      console.log(`claimed with an ${this.currentPlayer.symbol}`);
+      this.board[row][column] = this.currentPlayer.symbol;
+      this.currentPlayer = this.currentPlayer.otherPlayer;
     }
+    console.log(this.board)
   }
 }
--- src/player.js
+++ src/player.js
@@ -1,1 +1,8 @@
-console.log('Player is loaded')
+class Player{
+  constructor(symbol, icon, name, color){
+    this.symbol = symbol;
+    this.icon = icon;
+    this.name = name || 'Test-bot';
+    this.color = color || 'grey';
+  }
+}
