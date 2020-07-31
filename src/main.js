5146a4118ca52ea5c5905da2f2765cbc2d30302b
Matthew Lane <themplane@gmail.com>
2020-07-31 09:37:14 -0700

    Add click event that reports clicked tile. Add z-indexes to facilitate.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Main is loaded')
+var gameBoard = document.querySelector('#game-board');
+
+gameBoard.addEventListener('click', checkTile)
+
+function checkTile(event){
+  var tile = event.target.closest('.tile')
+  if (tile) {
+    console.log(tile.id)
+  }
+}
